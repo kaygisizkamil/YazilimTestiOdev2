70e92c1064cc632fd286720c37b6369e5f582fa1
kaygisizkamil <kamilkaygisizz@gmail.com>
2023-05-18 00:10:41 +0300

    Add tests for signup,also driver has updated since chrome also has been too

--- tests/systemTests/CheckBoxTests.java
+++ tests/systemTests/CheckBoxTests.java
@@ -1,8 +1,19 @@
-package systemTests;
+/**
+*
+* @author Kamil Kaygisiz kamil.kaygisiz1@ogr.sakarya.edu.tr
+* @since 9.05.2023
+* <p>
+* Sayfadaki left navbara tıklayınca acılan checkboxların islevselligi , duzgun calistigini teyit 
+* </p>
+*/
 
+package systemTests;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
 import org.openqa.selenium.JavascriptExecutor;
+
 import java.time.Duration;
 import java.util.List;
 
@@ -16,42 +27,34 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-public class CheckBoxTests {
-    private WebDriver driver;
-    private WebDriverWait wait;
 
+public class CheckBoxTests {
+	private WebDriver driver;
+	private WebDriverWait wait;
+	private int  pageHeight;
     @BeforeEach
-    public void setUp() {
+    public void setUp() throws InterruptedException {
         System.setProperty("webdriver.chrome.driver", "Drivers/chromedriver.exe");
         driver = new ChromeDriver();
         wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         driver.manage().window().maximize();
-        
         // Navigate to Amazon homepage
         driver.get("https://www.amazon.com");
-	      JavascriptExecutor executor = (JavascriptExecutor) driver;
-
+	    JavascriptExecutor executor = (JavascriptExecutor) driver;
+	    pageHeight = driver.manage().window().getSize().getHeight();
         // Click on Gift Cards link in the navbar
         WebElement navBar = driver.findElement(By.id("nav-main"));
-	      WebElement element = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[contains(text(),'Gift Cards')]")));
-	      executor.executeScript("arguments[0].click();", element);
-	      wait.until(ExpectedConditions.titleContains("Gift Cards"));
-
+	    WebElement element = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[contains(text(),'Gift Cards')]")));
+	    executor.executeScript("arguments[0].click();", element);
+	    wait.until(ExpectedConditions.titleContains("Gift Cards"));
+	    Thread.sleep(500);
 
     }
-
-    @AfterEach
-    public void tearDown() {
-        driver.quit();
-    }
-    /*
-     * This tests after chechbox selected does it directing me the true url and in 
-     * opened page if checkbox is really selected is done for two checkbox can be tested for them all
-     * but would take forever time so i leave it like this
-     * i got some help from chroPath chrome extension in here i could not find the checkboxes xPath easily it give me the exact true
-     *  and easy result
-     */
-    @Test
+	@AfterEach
+	public void tearDown() {
+		driver.quit();
+	}
+	@Test
     public void testCheckbox() throws InterruptedException {
     	String urlToBeReturned="https://www.amazon.com/gp/browse.html?node=2238192011&ref_=nav_em_hmc_gc_allgc_0_2_27_2";
         driver.get("https://www.amazon.com/gp/browse.html?node=2238192011&ref_=nav_em_hmc_gc_allgc_0_2_27_2");
@@ -80,6 +83,9 @@ public class CheckBoxTests {
         WebElement mailCardCheckbox = driver.findElement(mailCardCheckboxLocator);
         mailCardCheckbox.click();
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//body/div[@id='a-page']/div[@id='search']/span[1]")));
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("window.scrollBy(0, " + pageHeight/4 + ")");
+        Thread.sleep(500);
         driver.get("https://www.amazon.com/s?bbn=2238192011&rh=n%3A2238192011%2Cp_n_format_browse-bin%3A2740967011&dc&qid=1683765089&rnid=2740963011&ref=lp_2864120011_nr_p_n_format_browse-bin_1");        
         WebElement checkboxOnNewPage2 = driver.findElement(By.xpath("//body[1]/div[1]/div[2]/div[1]/div[2]/div[1]/div[3]/span[1]/div[1]/div[1]/div[1]/div[5]/ul[1]/span[2]/li[1]/span[1]/a[1]/div[1]/label[1]/i[1]"));
        // assertTrue(checkboxOnNewPage.isSelected());
@@ -91,4 +97,32 @@ public class CheckBoxTests {
         System.out.println("Test done successfully");
 
     }
+	/*Scenario:After clickin the Gifts get the num of total gifts
+	 * Click first amazon get the num of total amazon gifts compare them
+	 * should not be equal
+	 * 
+	 */
+    @Test
+    public void testNestedCheckbox() throws InterruptedException {
+        driver.get("https://www.amazon.com/s?i=gift-cards&bbn=2238192011&rh=n%3A2238192011%2Cp_n_format_browse-bin%3A2740964011&dc&ds=v1%3AzD7MwieAJFvsVUZxGKGcffaMRUoIJZja17r2%2FDIdidA&qid=1683763332&ref=sr_ex_n_1");        
+        By resultPath=By.xpath("//body/div[@id='a-page']/div[@id='search']/span[1]/div[1]/h1[1]/div[1]/div[1]/div[1]/div[1]/span[1]");
+        WebElement howMany = wait.until(ExpectedConditions.visibilityOfElementLocated(resultPath));
+        String resultText = howMany.getText();
+        int resultCount = Integer.parseInt(resultText.split(" ")[3].replace(",", ""));
+        
+        WebElement amazonCheckBox=wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//body[1]/div[1]/div[2]/div[1]/div[2]/div[1]/div[3]/span[1]/div[1]/div[1]/div[1]/div[3]/ul[1]/span[1]/li[1]/span[1]/a[1]/div[1]/label[1]/i[1]")));
+        amazonCheckBox.click();
+        Thread.sleep(1000);
+        WebElement howMany2=wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//body/div[@id='a-page']/div[@id='search']/span[1]/div[1]/h1[1]/div[1]/div[1]/div[1]")));
+        String resultText2 = howMany2.getText();
+        int resultCount2 = Integer.parseInt(resultText2.split(" ")[2].replace(",", ""));
+        
+        assertNotEquals(resultCount,resultCount2);
+            	
+    }
+	
+	
+	
 }
+ 
+
---
+++ tests/systemTests/Signuptests.java
@@ -0,0 +1,142 @@
+/**
+*
+* @author Kamil Kaygisiz kamil.kaygisiz1@ogr.sakarya.edu.tr
+* @since 9.05.2023
+* <p>
+* Sayfadaki sign up test edilir.Fake mail hizmeti veren siteler bile kullanılsa sitede ekstra koruma oldugu icin
+* hardcoded daha once kayitli mailler ile test edildi.
+* </p>
+*/
+
+package systemTests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.*;
+import org.openqa.selenium.support.ui.*;
+import org.junit.runners.MethodSorters;
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+import org.junit.jupiter.api.Test;
+
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class Signuptests {
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    @BeforeEach
+    public void setup() {
+        System.setProperty("webdriver.chrome.driver", "Drivers/chromedriver.exe");
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        driver.get("https://www.amazon.com/");
+    }
+
+    @AfterEach
+    public void cleanup() {
+        driver.quit();
+    }
+
+    @Test
+    public void testSuccessfulSignUp() {
+        navigateToSignUpPage();
+
+        String email = "example@example.com"; // Change the email to a valid email address
+        fillRegistrationForm("Kygsz kygsz", email, "yourpassword", "yourpassword");
+        submitForm();
+
+        String expectedUrl = "https://www.amazon.com/ap/register";
+        String actualUrl = driver.getCurrentUrl();
+        Assertions.assertEquals(expectedUrl, actualUrl, "Signup failed. Expected URL: " + expectedUrl + ", Actual URL: " + actualUrl);
+    }
+
+    @Test
+    public void testWeakPassword() {
+        navigateToSignUpPage();
+
+        String email = "example2@example.com"; // Change the email to a valid email address
+        fillRegistrationForm("Kygsz kygsz", email, "weak", "weak");
+        submitForm();
+
+        String expectedErrorMessage = "Minimum 6 characters required";
+        WebElement passwordError = driver.findElement(By.id("auth-password-invalid-password-alert"));
+        String actualErrorMessage = passwordError.getText().trim();
+        Assertions.assertEquals(expectedErrorMessage, actualErrorMessage, "Invalid error message. Expected: " + expectedErrorMessage + ", Actual: " + actualErrorMessage);
+    }
+
+    @Test
+    public void testInvalidEmail() {
+        navigateToSignUpPage();
+
+        fillRegistrationForm("Kygsz kygsz", "invalidemail", "yourpassword", "yourpassword");
+        submitForm();
+
+        String expectedErrorMessage = "Wrong or Invalid email address or mobile phone number. Please correct and try again.";
+        WebElement emailError = driver.findElement(By.xpath("//div[@id='auth-email-invalid-claim-alert']"));
+        String actualErrorMessage = emailError.getText().trim();
+        Assertions.assertEquals(expectedErrorMessage, actualErrorMessage, "Invalid error message. Expected: " + expectedErrorMessage + ", Actual: " + actualErrorMessage);
+    }
+
+    @Test
+    public void testExistingEmail() {
+        navigateToSignUpPage();
+
+        fillRegistrationForm("Kygsz kygsz", "existingemail@example.com", "yourpassword", "yourpassword");
+        submitForm();
+
+        String currentUrl = driver.getCurrentUrl();
+        String registrationPageUrl = "https://www.amazon.com/ap/register";
+
+        if (currentUrl.equals(registrationPageUrl)) {
+            WebElement emailError = driver.findElement(By.xpath("//body/div[@id='a-page']/div[1]/div[2]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]"));
+            String expectedErrorMessage = "An account with this email address already exists.";
+            String actualErrorMessage = emailError.getText().trim();
+            Assertions.assertEquals(expectedErrorMessage, actualErrorMessage, "Invalid error message. Expected: " + expectedErrorMessage + ", Actual: " + actualErrorMessage);
+        }else {
+            String protectYourAccountUrl = "https://www.amazon.com/ap/cvf/request?arb=e3bc0d4f-3e40-429b-a605-66de88845a8f";
+            Assertions.assertTrue(currentUrl.contains("https://www.amazon.com/ap/cvf/request?arb="), "Redirected to unexpected URL: " + currentUrl);           
+        }
+    }
+
+
+    private void navigateToSignUpPage() {
+        WebElement signInLink = driver.findElement(By.id("nav-link-accountList"));
+        signInLink.click();
+
+        WebElement createAccountButton = driver.findElement(By.id("createAccountSubmit"));
+        createAccountButton.click();
+    }
+
+    private void fillRegistrationForm(String name, String email, String password, String confirmPassword) {
+        WebElement nameField = driver.findElement(By.id("ap_customer_name"));
+        nameField.sendKeys(name);
+
+        WebElement emailField = driver.findElement(By.id("ap_email"));
+        emailField.sendKeys(email);
+
+        WebElement passwordField = driver.findElement(By.id("ap_password"));
+       
+        passwordField.sendKeys(password);
+
+        WebElement reEnterPasswordField = driver.findElement(By.id("ap_password_check"));
+        reEnterPasswordField.sendKeys(confirmPassword);
+    }
+
+    private void submitForm() {
+        WebElement createAccountSubmit = driver.findElement(By.id("continue"));
+        createAccountSubmit.click();
+    }
+
+
+}
