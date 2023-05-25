146cb5676fe2078b590aaa63d638137d9f1f8940
Kamil Kaygisiz <96066271+kaygisizkamil@users.noreply.github.com>
2023-05-26 02:50:51 +0300

    Update Signuptests.java

--- tests/systemTests/Signuptests.java
+++ tests/systemTests/Signuptests.java
@@ -51,102 +51,114 @@ public class Signuptests {
         driver.quit();
     }
     
-
-    @Test
+ @Test
     public void testSuccessfulSignUp() {
-    	driver.get("https://www.amazon.com");
-    	System.out.println("Sign-up testi basladi..");
-    	System.out.println();
+        System.out.println("Sign-up test started..");
         navigateToSignUpPage();
 
-        String email = "example@example.com"; 
+        String email = "example@example.com";
         fillRegistrationForm("Kygsz kygsz", email, "yourpassword", "yourpassword");
         submitForm();
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.urlToBe("https://www.amazon.com/ap/register"));
 
         String expectedUrl = "https://www.amazon.com/ap/register";
         String actualUrl = driver.getCurrentUrl();
         Assertions.assertEquals(expectedUrl, actualUrl, "Signup failed. Expected URL: " + expectedUrl + ", Actual URL: " + actualUrl);
+
+        System.out.println("Sign-up test completed.");
+        driver.quit();
     }
 
     @Test
     public void testWeakPassword() {
-    	driver.get("https://www.amazon.com");
-    	System.out.println("Weak password testi basladi..");
-    	System.out.println();
+        System.out.println("Weak password test started..");
         navigateToSignUpPage();
 
-        String email = "example2@example.com"; 
+        String email = "example2@example.com";
         fillRegistrationForm("Kygsz kygsz", email, "weak", "weak");
         submitForm();
 
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebElement passwordError = wait.until(ExpectedConditions.presenceOfElementLocated(By.id("auth-password-invalid-password-alert")));
+
         String expectedErrorMessage = "Minimum 6 characters required";
-        WebElement passwordError = driver.findElement(By.id("auth-password-invalid-password-alert"));
         String actualErrorMessage = passwordError.getText().trim();
         Assertions.assertEquals(expectedErrorMessage, actualErrorMessage, "Invalid error message. Expected: " + expectedErrorMessage + ", Actual: " + actualErrorMessage);
+
+        System.out.println("Weak password test completed.");
+        driver.quit();
     }
 
     @Test
     public void testInvalidEmail() {
-    	driver.get("https://www.amazon.com");
-    	System.out.println("Gecersiz email testi basladi..");
-    	System.out.println();
-
+        System.out.println("Invalid email test started..");
         navigateToSignUpPage();
 
         fillRegistrationForm("Kygsz kygsz", "invalidemail", "yourpassword", "yourpassword");
         submitForm();
 
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebElement emailError = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//div[@id='auth-email-invalid-claim-alert']")));
+
         String expectedErrorMessage = "Wrong or Invalid email address or mobile phone number. Please correct and try again.";
-        WebElement emailError = driver.findElement(By.xpath("//div[@id='auth-email-invalid-claim-alert']"));
         String actualErrorMessage = emailError.getText().trim();
         Assertions.assertEquals(expectedErrorMessage, actualErrorMessage, "Invalid error message. Expected: " + expectedErrorMessage + ", Actual: " + actualErrorMessage);
+
+        System.out.println("Invalid email test completed.");
+        driver.quit();
     }
 
     @Test
     public void testExistingEmail() {
-    	driver.get("https://www.amazon.com");
-    	System.out.println("Var-olan mail ile sign-up testi basladi..");
-    	System.out.println();
-
+        System.out.println("Existing email test started..");
         navigateToSignUpPage();
 
         fillRegistrationForm("Kygsz kygsz", "existingemail@example.com", "yourpassword", "yourpassword");
         submitForm();
 
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         String currentUrl = driver.getCurrentUrl();
         String registrationPageUrl = "https://www.amazon.com/ap/register";
 
         if (currentUrl.equals(registrationPageUrl)) {
-            WebElement emailError = driver.findElement(By.xpath("//body/div[@id='a-page']/div[1]/div[2]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]"));
+            WebElement emailError = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//body/div[@id='a-page']/div[1]/div[2]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]")));
+
             String expectedErrorMessage = "An account with this email address already exists.";
             String actualErrorMessage = emailError.getText().trim();
             Assertions.assertEquals(expectedErrorMessage, actualErrorMessage, "Invalid error message. Expected: " + expectedErrorMessage + ", Actual: " + actualErrorMessage);
-        }else {
+        } else {
             String protectYourAccountUrl = "https://www.amazon.com/ap/cvf/request?arb=e3bc0d4f-3e40-429b-a605-66de88845a8f";
-            Assertions.assertTrue(currentUrl.contains("https://www.amazon.com/ap/cvf/request?arb="), "Redirected to unexpected URL: " + currentUrl);           
+            Assertions.assertTrue(currentUrl.contains("https://www.amazon.com/ap/cvf/request?arb="), "Redirected to unexpected URL: " + currentUrl);
         }
+
+        System.out.println("Existing email test completed.");
+        driver.quit();
     }
 
-
     public void navigateToSignUpPage() {
-    	driver.get("https://www.amazon.com");
+        driver = new ChromeDriver();
+        driver.get("https://www.amazon.com");
+
         WebElement signInLink = driver.findElement(By.id("nav-link-accountList"));
         signInLink.click();
 
-        WebElement createAccountButton = driver.findElement(By.id("createAccountSubmit"));
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebElement createAccountButton = wait.until(ExpectedConditions.elementToBeClickable(By.id("createAccountSubmit")));
         createAccountButton.click();
     }
 
     public void fillRegistrationForm(String name, String email, String password, String confirmPassword) {
-    	driver.get("https://www.amazon.com");
-        WebElement nameField = driver.findElement(By.id("ap_customer_name"));
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        WebElement nameField = wait.until(ExpectedConditions.elementToBeClickable(By.id("ap_customer_name")));
         nameField.sendKeys(name);
 
         WebElement emailField = driver.findElement(By.id("ap_email"));
         emailField.sendKeys(email);
 
         WebElement passwordField = driver.findElement(By.id("ap_password"));
-       
         passwordField.sendKeys(password);
 
         WebElement reEnterPasswordField = driver.findElement(By.id("ap_password_check"));
